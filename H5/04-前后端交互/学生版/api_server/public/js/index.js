984d900e9170ddf596b395e5d07a69cc5db20dcb
Dwight Chan <9585869927@qq.com>
2019-12-16 00:16:10 +0800

    - 做缓存处理;

--- H5/04-前后端交互/学生版/api_server/public/js/index.js
+++ H5/04-前后端交互/学生版/api_server/public/js/index.js
@@ -1,8 +1,13 @@
 // const artTemplate = require("art-template");
 
-// import { template } from "express-art-template";
-// 导入art-tempate模板引擎
-// const template = require('art-template');
+// 本地存储的 key
+let dataKey = "employClass";
+// 1. 先加载上一次数据
+// 读取上一次数据
+
+readLastData();
+// 2. 接口请求
+// 
 $.ajax({
     // 请求地址
     url: '/home/employ-class',
@@ -11,10 +16,10 @@ $.ajax({
     // 请求成功之后回调函数
     success: function(response) {
 
-        console.log(response instanceof Array);
-        // 注意 这个模板名称不要和 id 相同
-        var html = template('employ-Class', { data: response });
-        $('#employClass').html(html);
+        // // 先渲染后保存
+        dwRenderUI(response);
+        // // 存取数据 在本地, 方便下次进入快速读取, (以字符串形式存储)
+        localStorage.setItem(dataKey, JSON.stringify(response));
     },
 
     // 请求失败函数回调
@@ -22,3 +27,19 @@ $.ajax({
         console.log(xhr);
     }
 });
+// 读取上一次的数据
+function readLastData() {
+    // 将保存的数据 string 类型 转为 对象
+    const response = JSON.parse(localStorage.getItem(dataKey));
+    console.log(JSON.stringify(response));
+    dwRenderUI(response);
+};
+// 渲染 UI 
+function dwRenderUI(response) {
+    console.log(response instanceof Array);
+    // 注意 这个模板名称不要和 id 相同
+    var html = template('employ-Class', { data: response });
+    console.log(html);
+
+    $('#employClass').html(html);
+}
