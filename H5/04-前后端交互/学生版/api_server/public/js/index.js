d0819904e65db82bac5f447fef153f7bcd2403cf
Dwight Chan <9585869927@qq.com>
2019-12-16 00:02:23 +0800

    --

--- H5/04-前后端交互/学生版/api_server/public/js/index.js
+++ H5/04-前后端交互/学生版/api_server/public/js/index.js
@@ -10,22 +10,10 @@ $.ajax({
     type: 'get',
     // 请求成功之后回调函数
     success: function(response) {
-        console.log(response);
-        response.forEach(model => {
-            console.log(model instanceof Object);
-            console.log(model.name + ":name");
-            console.log(model.img + ":img");
-            console.log(model.price + ":price");
-            console.log(model.count + ":count");
-            console.log(model.__v + ":__v");
-        });
-        console.log("是数组");
 
         console.log(response instanceof Array);
         // 注意 这个模板名称不要和 id 相同
         var html = template('employ-Class', { data: response });
-        console.log(html);
-        console.log(response);
         $('#employClass').html(html);
     },
 
