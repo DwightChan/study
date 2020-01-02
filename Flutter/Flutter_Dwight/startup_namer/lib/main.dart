c9221d9e94d417cfd0e03480e2af2c6ae245af65
Dwight <dwight@irainbow7.com>
2020-01-02 14:51:07 +0800

    第6步: 导航到新页面;

--- Flutter/Flutter_Dwight/startup_namer/lib/main.dart
+++ Flutter/Flutter_Dwight/startup_namer/lib/main.dart
@@ -55,11 +55,43 @@ class RandomWordsState extends State<RandomWords> {
       appBar: new AppBar(
         // 导航栏 title 
         title: new Text("Startup Name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved,)
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
-
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+            return new Scaffold(
+              appBar: new AppBar(
+                title: new Text('Saved Suggestions'),
+              ),
+              body: new ListView(children: divided),
+            );
+        },
+      ),
+    );
+  }
   Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(0.0),
