ff17925705781dc6d295570eb140b3d7413cefbf
Dwight <dwight@irainbow7.com>
2020-01-14 18:27:54 +0800

    - 学习代码 商品分类 doing;

--- H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/Cate.vue
+++ H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/Cate.vue
@@ -9,15 +9,125 @@
 
     <!-- 卡片视图区域 -->
     <el-card>
-      <!-- 警告区域 -->
-      <el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon :closable="false"> </el-alert>
+      <el-row>
+        <el-col>
+          <el-button type="primary" @click="showAddCateDialog"
+            >添加分类</el-button
+          >
+        </el-col>
+      </el-row>
     </el-card>
+
+    <!-- 添加分类的对话框 -->
+    <el-dialog
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClosed"
+    >
+      <!-- 添加分类的表单 -->
+      <el-form
+        :model="addCateForm"
+        :rules="addCateFormRules"
+        ref="addCateFormRef"
+        label-width="100%"
+      >
+        <el-form-item label="分类名称:" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类:">
+          <el-cascader
+            v-model="selectedKeys"
+            :options="cascaderProps"
+            :props="{ expandTrigger: 'hover', value: 'cascaderProps'}"
+            @change="parentCateChanged"
+            clearable
+            change-on-selected
+          ></el-cascader>
+        </el-form-item>
+        <el-form-item label="分类名称:" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      // 查询条件
+      queryInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类的数据列表, 默认为空
+      catelist: [],
+      // 总数据条数
+      total: 0,
+      // 为table指定列的定义
+      columns: [
+        {
+          label: '分类名称',
+          prop: 'cat_name'
+        }
+      ],
+      // 控制添加分类对话框的显示与隐藏
+      addCateDialogVisible: false,
+      // 添加分类的表单数据对象
+      addCateForm: {
+        // 将要添加的分类的名称
+        cat_name: '',
+        // 父级分类的 id
+        cat_pid: 0,
+        // 分类的等级, 默认要添加的是1级分类
+        cat_level: 0
+      },
+      // 添加分类表单的验证规则
+      addCateFormRules: {
+        cat_name: [
+          { required: true, message: '请输入分类名称', trigger: 'blur' }
+        ],
+        // 父级分类的列表
+        parentCateList: [],
+        // 指定级联选择器的配置对象
+        cascaderProps: {
+          value: 'cat_id',
+          label: 'cat_name',
+          children: 'children'
+        },
+        // 选中的父级分类的Id数组
+        selectedKeys: []
+      }
+    }
+  },
   created() {
     console.log('分类管理页面')
+  },
+  methods: {
+    showAddCateDialog() {
+      this.addCateDialogVisible = true
+    },
+    // 监听对话框的关闭事件, 充值表单数据
+    addCateDialogClosed() {},
+    // 选择项发生变化触发这个函数
+    parentCateChanged() {
+      console.log(this.selectedKeys)
+      // 如果 selectedKeys 数组中的 length 大于0，证明选中的父级分类
+      // 反之，就说明没有选中任何父级分类
+      if (this.selectedKeys.length > 0) {
+        // 父级分类的Id
+        this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
+        // 为当前分类的等级赋值
+        this.addCateForm.cat_level = this.selectedKeys.length
+      } else {
+        // 父级分类的Id
+        this.addCateForm.cat_pid = 0
+        // 为当前分类的等级赋值
+        this.addCateForm.cat_level = 0
+      }
+    }
   }
 }
 </script>
