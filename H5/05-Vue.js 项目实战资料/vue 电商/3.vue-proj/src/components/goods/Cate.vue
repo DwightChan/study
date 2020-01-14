b2d55c40008a7748c5716f868f03da6fbf3ef31c
Dwight <dwight@irainbow7.com>
2020-01-14 10:01:47 +0800

    - goods 相关页面;

---
+++ H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/Cate.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3>分类管理</h3>
+  </div>
+</template>
+<script>
+export default {
+
+  created () {
+    console.log('分类管理页面')
+  }
+}
+</script>
+<style lang="less" scoped></style>
---
+++ H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/Params.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>分类参数</h3>
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    console.log('分类参数页面')
+  }
+}
+</script>
+<style lang="less" scoped></style>
