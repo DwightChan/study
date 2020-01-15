ab829f7f6de1797f8dbb9e3ec1ce9ac23d2c68b8
Dwight <dwight@irainbow7.com>
2020-01-15 16:56:34 +0800

    - 添加了 时间过滤器 - 商品列表 基本完成;

--- H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/List.vue
+++ H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/List.vue
@@ -9,14 +9,140 @@
 
     <!-- 卡片视图区域 -->
     <el-card>
-      123
+      <!-- 每行内列与列之间的间隙 -->
+      <el-row :gutter="20">
+        <!-- 占比 8 / 24 -->
+        <el-col :span="8">
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            class="input-with-select"
+            clearable
+          >
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary">添加商品</el-button>
+        </el-col>
+      </el-row>
+      <!-- table 表格区域 -->
+      <el-table :data="goodslist" border stripe>
+        <el-table-column type="index" label="序号"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column
+          label="商品价格(元)"
+          prop="goods_price"
+          width="90px"
+        ></el-table-column>
+        <el-table-column
+          label="商品重量"
+          prop="goods_weight"
+          width="75px"
+        ></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140px">
+          <template slot-scope="scope">
+            {{ scope.row.add_time | dateFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template slot-scope="scope">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+            ></el-button>
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="mini"
+              @click="removeById(scope.row.goods_id)"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      // 查询参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      // 商品列表
+      goodslist: [],
+      // 总数据条数
+      total: 0
+    }
+  },
   created() {
     console.log('商品列表页面')
+    this.getGoodsList()
+  },
+  methods: {
+    // 根据分页获取对应的商品列表
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('goods', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品列表失败! ')
+      }
+      this.$message.success('获取商品列表成功!')
+      console.log(res.data)
+      this.goodslist = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getGoodsList()
+    },
+    handleCurrentChange(num) {
+      this.queryInfo.pagenum = num
+      this.getGoodsList()
+    },
+    async removeById(id) {
+      console.log(id)
+
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该商品, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
+
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已经取消删除!')
+      }
+      const { data: res } = await this.$http.delete(`goods/${id}`)
+
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除失败!')
+      }
+      this.$message.success('删除成功!')
+      this.getGoodsList()
+    }
   }
 }
 </script>
