263e9300725043f4641daaa2fed4a9aa4d372e7b
Dwight <dwight@irainbow7.com>
2020-01-15 15:09:36 +0800

    - 导入 import TreeTable from 'vue-table-with-tree-grid' 制作 cate.vue 页面的表格; - 完成 分类列表的 表格数据展示与添加;

--- H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/Cate.vue
+++ H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/components/goods/Cate.vue
@@ -16,6 +16,53 @@
           >
         </el-col>
       </el-row>
+      <!-- 表格 -->
+      <tree-table
+        class="treeTable"
+        :data="catelist"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        show-index
+        index-text="序号"
+        border
+        :show-row-hover="false"
+      >
+        <!-- 是否有效 -->
+        <template slot="isok" slot-scope="scope">
+          <i
+            class="el-icon-success"
+            v-if="scope.row.cat_deleted === false"
+            style="color: lightgreen;"
+          ></i>
+          <i class="el-icon-error" v-else style="color: red;"></i>
+        </template>
+        <!-- 排序 -->
+        <template slot="order" slot-scope="scope">
+          <el-tag size="mini" v-if="scope.row.cat_level == 0">一级</el-tag>
+          <el-tag size="mini" type="success" v-if="scope.row.cat_level == 1"
+            >二级</el-tag
+          >
+          <el-tag size="mini" type="warning" v-if="scope.row.cat_level == 2"
+            >三级</el-tag
+          >
+        </template>
+        <template slot="opt">
+          <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+        </template>
+      </tree-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="pageSizes"
+        :page-size="queryInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+        background
+      >
+      </el-pagination>
     </el-card>
 
     <!-- 添加分类的对话框 -->
@@ -30,25 +77,36 @@
         :model="addCateForm"
         :rules="addCateFormRules"
         ref="addCateFormRef"
-        label-width="100%"
+        width="50%"
+        label-width="100px"
       >
         <el-form-item label="分类名称:" prop="cat_name">
           <el-input v-model="addCateForm.cat_name"></el-input>
         </el-form-item>
         <el-form-item label="父级分类:">
-          <el-cascader
+          <!-- <el-cascader
             v-model="selectedKeys"
             :options="cascaderProps"
-            :props="{ expandTrigger: 'hover', value: 'cascaderProps'}"
+            :props="cascaderProps"
             @change="parentCateChanged"
             clearable
-            change-on-selected
+            change-on-select
+          ></el-cascader> -->
+          <!-- <el-cascader expand-trigger="hover" :options="parentCateList" :props="cascaderProps" v-model="selectedKeys" @change="parentCateChanged" clearable change-on-select> -->
+          <!-- </el-cascader> -->
+          <el-cascader
+            max-height="80%"
+            v-model="selectedKeys"
+            :props="cascaderProps"
+            @change="parentCateChanged"
+            :options="parentCateList"
           ></el-cascader>
         </el-form-item>
-        <el-form-item label="分类名称:" prop="cat_name">
-          <el-input v-model="addCateForm.cat_name"></el-input>
-        </el-form-item>
       </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
     </el-dialog>
   </div>
 </template>
@@ -58,12 +116,15 @@ export default {
     return {
       // 查询条件
       queryInfo: {
+        // 接口参数 3级
         type: 3,
         pagenum: 1,
         pagesize: 5
       },
       // 商品分类的数据列表, 默认为空
       catelist: [],
+      // 每页显示个数选择器的选项设置
+      pageSizes: [1, 3, 5, 15],
       // 总数据条数
       total: 0,
       // 为table指定列的定义
@@ -71,6 +132,29 @@ export default {
         {
           label: '分类名称',
           prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 表示当前列表定位模板礼列表
+          type: 'template',
+          // 标识当前这个列表使用模板名称是 isok
+          template: 'isok'
+        },
+        {
+          label: '排序',
+          // 标识当前列表为模板列表
+          type: 'template',
+          // 表示当前这一列使用的模板名称 order
+          template: 'order'
+        },
+        {
+          label: '操作',
+          // 表示将当前列定义为模板列
+          type: 'template',
+          // 表示当前这一列使用的模板名称 opt
+          template: 'opt',
+          // 列最小宽度
+          minWidth: '180px'
         }
       ],
       // 控制添加分类对话框的显示与隐藏
@@ -88,29 +172,71 @@ export default {
       addCateFormRules: {
         cat_name: [
           { required: true, message: '请输入分类名称', trigger: 'blur' }
-        ],
-        // 父级分类的列表
-        parentCateList: [],
-        // 指定级联选择器的配置对象
-        cascaderProps: {
-          value: 'cat_id',
-          label: 'cat_name',
-          children: 'children'
-        },
-        // 选中的父级分类的Id数组
-        selectedKeys: []
-      }
+        ]
+      },
+      // 父级分类的列表
+      parentCateList: [],
+      // 指定级联选择器的配置对象
+      cascaderProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children',
+        expandTrigger: 'hover'
+      },
+      // 选中的父级分类的Id数组
+      selectedKeys: []
     }
   },
   created() {
     console.log('分类管理页面')
+    // 获取当前的分类列表数据
+    this.getCateList()
+    // 先获取父级分类的数据列表
+    this.dwGetParentCateList()
   },
   methods: {
+    // 获取山炮分类数据
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取商品分类失败!')
+      }
+      console.log(res.data)
+      // 把数据列表, 赋值给 cateList
+      this.catelist = res.data.result
+      // 为总数据条数赋值
+      this.total = res.data.total
+    },
+    // 监听pagesize 变化
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getCateList()
+    },
+    // 监听 pagenum 改变
+    handleCurrentChange(newNum) {
+      this.queryInfo.pagenum = newNum
+      this.getCateList()
+    },
     showAddCateDialog() {
+      // 先获取父级分类的数据列表
+      this.dwGetParentCateList()
+      // 在展示对话框
       this.addCateDialogVisible = true
     },
-    // 监听对话框的关闭事件, 充值表单数据
-    addCateDialogClosed() {},
+    // 获取父级分类的数据列表
+    async dwGetParentCateList() {
+      const { data: res } = await this.$http.get('categories', {
+        params: { type: 2 }
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取父级分类数据失败!')
+      }
+      console.log(res)
+      this.parentCateList = res.data
+      console.log(this.parentCateList)
+    },
     // 选择项发生变化触发这个函数
     parentCateChanged() {
       console.log(this.selectedKeys)
@@ -118,7 +244,9 @@ export default {
       // 反之，就说明没有选中任何父级分类
       if (this.selectedKeys.length > 0) {
         // 父级分类的Id
-        this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
+        let index = this.selectedKeys.length - 1
+        this.addCateForm.cat_pid = this.selectedKeys[index]
+
         // 为当前分类的等级赋值
         this.addCateForm.cat_level = this.selectedKeys.length
       } else {
@@ -127,8 +255,41 @@ export default {
         // 为当前分类的等级赋值
         this.addCateForm.cat_level = 0
       }
+    },
+    // 点击按钮，添加新的分类
+    addCate() {
+      this.$refs.addCateFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post(
+          'categories',
+          this.addCateForm
+        )
+
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加分类失败')
+        }
+        this.$message.success('添加分类成功!')
+        this.addCateDialogVisible = false
+      })
+    },
+    // 监听对话框的关闭事件, 充值表单数据
+    addCateDialogClosed() {
+      this.$refs.addCateFormRef.resetFields()
+      this.selectedKeys = []
+      this.addCateForm.cat_level = 0
+      this.addCateForm.cat_pid = 0
+      this.addCateForm.cat_name = ''
     }
   }
 }
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.treeTable {
+  margin-top: 15px;
+  // margin-left: 10px;
+  // margin-right: 10px;
+}
+.el-cascader-menu {
+  height: 300px;
+}
+</style>
--- H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/router.js
+++ H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/router.js
@@ -7,7 +7,7 @@ import Welcome from './components/Welcome.vue'
 import Users from './components/user/Users.vue'
 import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
-import Categories from './components/goods/Cate.vue'
+import Cate from './components/goods/Cate.vue'
 import Params from './components/goods/Params.vue'
 import GoodsList from './components/goods/List.vue'
 
@@ -26,7 +26,7 @@ const router = new Router({
         { path: '/users', component: Users },
         { path: '/rights', component: Rights },
         { path: '/roles', component: Roles },
-        { path: '/categories', component: Categories },
+        { path: '/categories', component: Cate },
         { path: '/params', component: Params },
         { path: '/goods', component: GoodsList }
       ]
