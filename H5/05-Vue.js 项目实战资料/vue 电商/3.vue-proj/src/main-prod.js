cdce46b6d0c1db7dc5a976b05feef2fa20d8d4c1
Dwight <958689927@qq.com>
2020-01-27 15:44:59 +0800

    - dist 生成并 复制到 vue_api_server 项目中做静态资源测试;

--- H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/main-prod.js
+++ H5/05-Vue.js 项目实战资料/vue 电商/3.vue-proj/src/main-prod.js
@@ -1,40 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
-// 导入全局样式表
-import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
-
-// 导入进度条插件
-import NProgress from 'nprogress'
-// 导入进度条样式
-import 'nprogress/nprogress.css'
 
 // 导入富文本编辑器
 import VueQuillEditor from 'vue-quill-editor'
-// require styles 导入富文本编辑器对应的样式
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
+
+// 导入 NProgress 包对应的JS和CSS
+import NProgress from 'nprogress'
 
 import axios from 'axios'
-// 配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8889/api/private/v1/'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'http://127.0.0.1:8899/api/private/v1/'
+// 在 request 拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
-  // 当进入 request拦截器, 表示发送了请求, 我们就开启进度条
+  // console.log(config)
   NProgress.start()
-  // console.log('请求前 authorization:' + config.toString())
   config.headers.Authorization = window.sessionStorage.getItem('token')
-  // console.log('请求前 authorization:' + config.headers.Authorization)
   // 在最后必须 return config
   return config
 })
-// 在response 拦截器中, 隐藏进度条
+// 在 response 拦截器中，隐藏进度条 NProgress.done()
 axios.interceptors.response.use(config => {
-  // 当进入股 response 拦截器, 表示请求已经结束, 我们就结束进度条
   NProgress.done()
   return config
 })
@@ -43,11 +34,9 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
-
 // 将富文本编辑器，注册为全局可用的组件
 Vue.use(VueQuillEditor)
 
-// 自定义时间过滤器
 Vue.filter('dateFormat', function(originVal) {
   const dt = new Date(originVal * 1000)
   const y = dt.getFullYear()
