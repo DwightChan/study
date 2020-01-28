1bee5fe8996be89a8f9c099d4fde7c9df226c3d9
Dwight <958689927@qq.com>
2020-01-28 23:15:18 +0800

    - vue 作业: H5 项目;

---
+++ H5/05-Vue.js 项目实战资料/作业/博学谷头条/bxg_news_h5/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>这是三级标题</h3>
+    h5 首页
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
