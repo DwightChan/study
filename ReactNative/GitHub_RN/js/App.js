aac1ce9f61aad2c00333be636935df00fc3bea35
Dwight <958689927@qq.com>
2020-02-13 20:38:20 +0800

    - redux 集成开发指南--上;

--- ReactNative/GitHub_RN/js/App.js
+++ ReactNative/GitHub_RN/js/App.js
@@ -9,11 +9,14 @@
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
 import AppNavigators from "./navigator/AppNavigators";
+import store from "./store";
 
 export default class App extends Component {
   render() {
-    //
-    return <Provider>
+    /**
+     * 3. 将store 传递给app 框架
+     */
+    return <Provider store={store}>
       <AppNavigators/>
     </Provider>
   }
