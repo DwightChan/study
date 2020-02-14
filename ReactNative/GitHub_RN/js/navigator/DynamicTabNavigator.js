15e8d3d8f2e7d086f1fffc0642c7ffeba7dd59e0
Dwight <dwight@irainbow7.com>
2020-02-14 11:50:11 +0800

    - redux 集成开发--下;

--- ReactNative/GitHub_RN/js/navigator/DynamicTabNavigator.js
+++ ReactNative/GitHub_RN/js/navigator/DynamicTabNavigator.js
@@ -11,6 +11,7 @@ import GamePage from '../page/GamePage';
 import MaterialIcons from "react-native-vector-icons/MaterialIcons"
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Entypo from 'react-native-vector-icons/Entypo';
+import { connect } from "react-redux";
 
 const TABS = { // 这里配合页面的路由
   PopularPage: {
@@ -67,12 +68,15 @@ const TABS = { // 这里配合页面的路由
   },
 };
 
-export default class DynamicTabNaivgator extends Component {
+class DynamicTabNaivgator extends Component {
   constructor(props) {
     super(props);
     console.disableYellowBox = true; // 关闭黄色警告弹框;
   }
   _tabNavigator() {
+    if (this.Tabs) {
+      return this.Tabs;
+    }
     const {PopularPage, TrendingPage, FavoritePage, MyPage} = TABS;
     const tabs = {GamePage: {
       screen: GamePage,
@@ -95,13 +99,15 @@ export default class DynamicTabNaivgator extends Component {
       }
     }, PopularPage, TrendingPage, FavoritePage, MyPage};
     PopularPage.navigationOptions.tabBarLabel = '最热123'; // 动态修改 tab 属性
-    return createAppContainer(createBottomTabNavigator(
+    return this.Tabs = createAppContainer(createBottomTabNavigator(
       // tabs,
       {
         ...tabs
       },
       {
-        tabBarComponent: TabBarComponent,
+        tabBarComponent: props => {
+          return <TabBarComponent theme={this.props.theme} {...props}/>;
+        },
       }
     ));
   }
@@ -114,31 +120,32 @@ export default class DynamicTabNaivgator extends Component {
 }
 
 class TabBarComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.theme = {
-      tintColor: props.activeTintColor,
-      updateTime: new Date().getTime(),
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.theme = {
+  //     tintColor: props.activeTintColor,
+  //     updateTime: new Date().getTime(),
+  //   };
+  // }
   render() {
-    const {routes, index} = this.props.navigation.state;
-    console.log(routes);
-    console.log('----routes----');
-    if (routes[index].params) {
-      const {theme} = routes[index].params;
-      // 以最新的 更新时间为主, 频繁被其他tab 之前的修改覆盖
-      if (theme && theme.updateTime > this.theme.updateTime) {
-        console.log(index);
-        console.log('----index----');
-        console.log(theme);
-        this.theme = theme;
-        // this.setTheme(...theme);
-      }
-    }
+    // const {routes, index} = this.props.navigation.state;
+    // console.log(routes);
+    // console.log('----routes----');
+    // if (routes[index].params) {
+    //   const {theme} = routes[index].params;
+    //   // 以最新的 更新时间为主, 频繁被其他tab 之前的修改覆盖
+    //   if (theme && theme.updateTime > this.theme.updateTime) {
+    //     console.log(index);
+    //     console.log('----index----');
+    //     console.log(theme);
+    //     this.theme = theme;
+    //     // this.setTheme(...theme);
+    //   }
+    // }
     return <BottomTabBar
       {...this.props}
-      activeTintColor={this.theme.tintColor || this.props.activeTintColor}
+      // activeTintColor={this.theme.tintColor || this.props.activeTintColor}
+      activeTintColor={this.props.theme}
     />;
   }
 }
@@ -156,3 +163,16 @@ const styles = StyleSheet.create({
       margin: 10,
   },
 });
+
+// 在动态 daynamicTabNavigator 使用这个 
+const mapStateToProps = state => ({
+  theme: state.theme.theme,
+});
+export default connect(mapStateToProps)(DynamicTabNaivgator);
+
+// 这页面中使用这个
+// const mapDispatchToProps = dispatch => ({
+//   onThemeChange: theme => dispatch(actions.onThemeChange(theme)),
+// });
+
+// export default connect(null, mapDispatchToProps)(DynamicTabNaivgator);
--- ReactNative/GitHub_RN/js/page/MyPage.js
+++ ReactNative/GitHub_RN/js/page/MyPage.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
+import actions from "../action";
+import { connect } from "react-redux";
 
-export default class MyPage extends Component {
+class MyPage extends Component {
     render() {
         const{navigation} = this.props;
         return (
@@ -10,14 +12,7 @@ export default class MyPage extends Component {
                     MyPage
                 </Text>
                 <Button title={'修改主题'}
-                onPress={() => navigation.setParams(
-                  {
-                    theme: {
-                      tintColor: '#111',
-                      updateTime: new Date().getTime(),
-                    }
-                  },
-                )}
+                  onPress={() => this.props.onThemeChange('skyblue')}
                 />
             </View>
         )
@@ -37,3 +32,10 @@ const styles = StyleSheet.create({
         margin: 10,
     },
 });
+
+const mapDispatchToProps = dispatch => ({
+  onThemeChange: theme => dispatch(actions.onThemeChange(theme)),
+});
+
+export default connect(null, mapDispatchToProps)(MyPage);
+
