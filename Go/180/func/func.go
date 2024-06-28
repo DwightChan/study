3fc549d214b0a7822e07a211026827315bbb9a18
hqxc-Dwight <hqxc.dwight@gmail.com>
2024-06-28 18:45:43 +0800

    - 函数；

---
+++ Go/180/func/func.go
@@ -0,0 +1,99 @@
+/*
+ * @Author: hqxc-Dwight hqxc.dwight@gmail.com
+ * @Date: 2024-06-28 18:22:06
+ * @LastEditors: hqxc-Dwight hqxc.dwight@gmail.com
+ * @LastEditTime: 2024-06-28 18:43:30
+ * @FilePath: /study/Go/180/func/func.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+package main
+
+/**
+函数 语法要点回顾
+1. 返回值类型写在最后面
+2. 可返回多个值
+3. 函数做为参数
+4. 没有默认参数，可选参数
+*/
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
+
+func eval(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		q, _ := div(a, b)
+		return q, nil
+	default:
+		return 0, fmt.Errorf(
+			"unsupported operation: %s", op)
+	}
+}
+
+func div(a, b int) (q, r int) {
+	return a / b, a % b
+}
+
+func apply(op func(int, int) int, a, b int) int {
+
+	// 通过反射或者 函数名称
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %s with args "+
+		"(%d, %d)\n", opName, a, b)
+
+	return op(a, b)
+}
+
+func sum(numbers ...int) int {
+	s := 0
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func swap(a, b int) (int, int) {
+	return b, a
+}
+
+func main() {
+
+	if result, err := eval(3, 4, "x"); err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println(result)
+	}
+
+	q, r := div(13, 3)
+	fmt.Println(q, r)
+
+	fmt.Println((apply(pow, 3, 4)))
+
+	fmt.Println(apply(
+		func(a int, b int) int {
+			return int(math.Pow(
+				float64(a), float64(b)))
+		}, 3, 4))
+
+	fmt.Println(sum(1, 2, 3, 4, 5))
+
+	a, b := 3, 4
+	a, b = swap(a, b)
+	fmt.Println(a, b)
+}
