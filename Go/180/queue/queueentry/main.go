84b86ae50bd3f27df23b532f8e02d3be12b84b88
Dwight <Dwight@gmail.com>
2024-07-01 04:46:26 +0800

    - 目录文件整理；

--- Go/180/queue/queueentry/main.go
+++ Go/180/queue/queueentry/main.go
@@ -2,14 +2,14 @@
  * @Author: Dwight Dwight@gmail.com
  * @Date: 2024-07-01 03:29:17
  * @LastEditors: Dwight Dwight@gmail.com
- * @LastEditTime: 2024-07-01 03:30:50
+ * @LastEditTime: 2024-07-01 04:38:09
  * @FilePath: /study/Go/180/container/queue/queueentry/main.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 package main
 
 import (
-	"180/container/queue"
+	"180/queue"
 	"fmt"
 )
 
