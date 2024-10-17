8f20b15142f0498b1c9f9cece200486a3c4a921b
Dwight <Dwight@gmail.com>
2024-10-17 21:14:19 +0800

    - 使用插件 flutter-color-plugin

--- Flutter/flutter_base_demo/lib/plugin_use.dart
+++ Flutter/flutter_base_demo/lib/plugin_use.dart
@@ -1,38 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_base_demo/plugin_use.dart';
+import 'package:flutter_color_plugin/flutter_color_plugin.dart';
 
-void main() {
-  runApp(const PluginUse());
-}
+class PluginUse extends StatelessWidget {
+  const PluginUse({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '如何使用Flutter包和插件？',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: '初始化基础项目'),
+      home: MyHomePage(title: '如何使用Flutter包和插件？'),
     );
   }
 }
